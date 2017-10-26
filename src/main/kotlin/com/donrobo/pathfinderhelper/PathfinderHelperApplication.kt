7ab92b296908f70a27d534d822ee1f9410abc20d
Robert Tieber <robert.tieber@gmail.com>
2017-10-27 00:38:36 +0200

    Initial spring project

---
+++ src/main/kotlin/com/donrobo/pathfinderhelper/PathfinderHelperApplication.kt
@@ -0,0 +1,11 @@
+package com.donrobo.pathfinderhelper
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class PathfinderHelperApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(PathfinderHelperApplication::class.java, *args)
+}
---
+++ src/test/kotlin/com/donrobo/pathfinderhelper/PathfinderHelperApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.donrobo.pathfinderhelper
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class PathfinderHelperApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
